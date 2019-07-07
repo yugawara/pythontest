2521393dfc7276e530c306164dec5dae4b4971fe
Yasuaki Kudo <yasu@yasuaki.com>
2019-07-07 20:02:37 +0900

    http request

--- myscript.py
+++ myscript.py
@@ -1,8 +1,28 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i python3 -p "python37.withPackages(ps: with ps; [tornado numpy])"
+#! nix-shell -i python3 -p "python37.withPackages(ps: with ps; [tornado])"
 
-import numpy
-import tornado
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
 
-print(numpy.__version__)
-print(tornado.version)
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([(r"/", MainHandler)])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
